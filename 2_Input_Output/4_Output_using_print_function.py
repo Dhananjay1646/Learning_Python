e889689d2f7ef73013879ab4e7421b0651032abc
Dhananjay <23612018+Dhananjay1646@users.noreply.github.com>
2020-09-26 02:00:37 +0530

    Update 4_Output_using_print_function.py

--- 2_Input_Output/4_Output_using_print_function.py
+++ 2_Input_Output/4_Output_using_print_function.py
@@ -61,7 +61,9 @@ print("\n***********************************************************************
 
 ## sep parameter in print()
 
-# The separator between the arguments to print() function in Python is space by default (softspace feature), which can be modified and can be made to any character, integer or string as per our choice. The 'sep' parameter is used to achieve the same, it is found only in python 3.x or later. It is also used for formatting the output strings.
+# The separator between the arguments to print() function in Python is space by default (softspace feature), which can be modified
+# and can be made to any character, integer or string as per our choice. The 'sep' parameter is used to achieve the same, it is found
+# only in python 3.x or later. It is also used for formatting the output strings.
 
 # Example 1:- 
 
