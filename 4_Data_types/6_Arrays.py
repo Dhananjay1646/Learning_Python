4fd6102006a8cae18ad2314e1ecd665fb44aede3
Dhananjay <23612018+Dhananjay1646@users.noreply.github.com>
2020-10-24 17:06:36 +0530

    Updated_Array.py

--- 4_Data_types/6_Arrays.py
+++ 4_Data_types/6_Arrays.py
@@ -9,14 +9,21 @@ print("\n***********************************************************************
 ### Python Arrays ###
 print("\n### Python Arrays ###\n")
 
-# An array is a collection of items stored at contiguous memory locations. The idea is to store multiple items of the same type together. This makes it easier to calculate the position of each element by simply adding an offset to a base value, i.e., the memory location of the first element of the array (generally denoted by the name of the array).
+# An array is a collection of items stored at contiguous memory locations. The idea is to store multiple items of the same type together.
+# This makes it easier to calculate the position of each element by simply adding an offset to a base value, i.e., the memory location of
+# the first element of the array (generally denoted by the name of the array).
 
-# For simplicity, we can think of an array a fleet of stairs where on each step is placed a value (let's say one of your friends). Here, you can identify the location of any of your friends by simply knowing the count of the step they are on. Array can be handled in Python by a module named array. They can be useful when we have to manipulate only a specific data type values. A user can treat lists as arrays. However, user cannot constraint the type of elements stored in a list. If you create arrays using the array module, all elements of the array must be of the same type.
+# For simplicity, we can think of an array a fleet of stairs where on each step is placed a value (let's say one of your friends). Here,
+# you can identify the location of any of your friends by simply knowing the count of the step they are on. Array can be handled in
+# Python by a module named array. They can be useful when we have to manipulate only a specific data type values. A user can treat lists
+# as arrays. However, user cannot constraint the type of elements stored in a list. If you create arrays using the array module, all
+# elements of the array must be of the same type.
 
 # Creating a Array
 print("# Creating a Array")
 
-# Array in Python can be created by importing array module. array(data_type, value_list) is used to create an array with data type and value list specified in its arguments.
+# Array in Python can be created by importing array module. array(data_type, value_list) is used to create an array with data type and
+# value list specified in its arguments.
 
 # Python program to demonstrate Creation of Array 
 
@@ -39,14 +46,15 @@ b = arr.array('d', [2.5, 3.2, 3.3])
 print ("The new created array is : ", end =" ") 
 for i in range (0, 3): 
 	print (b[i], end =" ") 
-	
 
 print("\n********************************************************************************************************************************")
 
 # Adding Elements to a Array
 print("\n# Adding Elements to a Array:-\n")
 
-# Elements can be added to the Array by using built-in insert() function. Insert is used to insert one or more data elements into an array. Based on the requirement, a new element can be added at the beginning, end, or any given index of array. append() is also used to add the value mentioned in its arguments at the end of the array.
+# Elements can be added to the Array by using built-in insert() function. Insert is used to insert one or more data elements into an
+# array. Based on the requirement, a new element can be added at the beginning, end, or any given index of array. append() is also used
+# to add the value mentioned in its arguments at the end of the array.
 
 # Python program to demonstrate Adding Elements to a Array 
 
@@ -55,7 +63,6 @@ import array as arr
 
 # array with int type 
 a = arr.array('i', [1, 2, 3]) 
-
 
 print ("Array before insertion : ", end =" ") 
 for i in range (0, 3): 
@@ -89,7 +96,6 @@ for i in (b):
 print()
 
 print("type(b):- ", type(b))
-
 
 print("\n********************************************************************************************************************************")
 
@@ -127,7 +133,10 @@ print("\n***********************************************************************
 # Removing Elements from the Array
 print("\n# Removing Elements from the Array\n")
 
-# Elements can be removed from the array by using built-in remove() function but an Error arises if element doesn't exist in the set. Remove() method only removes one element at a time, to remove range of elements, iterator is used. pop() function can also be used to remove and return an element from the array, but by default it removes only the last element of the array, to remove element from a specific position of the array, index of the element is passed as an argument to the pop() method.
+# Elements can be removed from the array by using built-in remove() function but an Error arises if element doesn't exist in the set.
+# Remove() method only removes one element at a time, to remove range of elements, iterator is used. pop() function can also be used to
+# remove and return an element from the array, but by default it removes only the last element of the array, to remove element from a
+# specific position of the array, index of the element is passed as an argument to the pop() method.
 
 # Note - Remove method in List will only remove the first occurrence of the searched element.
 
@@ -166,13 +175,16 @@ print ("The array after removing is : ", end ="")
 for i in range (0, 3): 
 	print (arr[i], end =" ") 
 
-
 print("\n********************************************************************************************************************************")
 
 # Slicing of a Array
 print("\n# Slicing of a Array\n")
 
-# In Python array, there are multiple ways to print the whole array with all the elements, but to print a specific range of elements from the array, we use Slice operation. Slice operation is performed on array with the use of colon(:). To print elements from beginning to a range use [:Index], to print elements from end use [:-Index], to print elements from specific Index till the end use [Index:], to print elements within a range, use [Start Index:End Index] and to print whole List with the use of slicing operation, use [:]. Further, to print whole array in reverse order, use [::-1].
+# In Python array, there are multiple ways to print the whole array with all the elements, but to print a specific range of elements from
+# the array, we use Slice operation. Slice operation is performed on array with the use of colon(:). To print elements from beginning to
+# a range use [:Index], to print elements from end use [:-Index], to print elements from specific Index till the end use [Index:], to
+# print elements within a range, use [Start Index:End Index] and to print whole List with the use of slicing operation, use [:]. Further,
+# to print whole array in reverse order, use [::-1].
 
 # Python program to demonstrate silicing of elements in a Array 
 
@@ -233,7 +245,6 @@ Sliced_array = a[::3]
 print("\nPrinting all elements with gap of 2 using slice operation:") 
 print(Sliced_array)
 
-
 print("\n********************************************************************************************************************************")
 
 # Searching element in a Array
@@ -242,7 +253,6 @@ print("\n# Searching element in a Array\n")
 # In order to search an element in the array we use a python in-built index() method. This function returns the index of the first occurrence of value mentioned in arguments.
 
 # Python code to demonstrate searching an element in array 
-
 
 # importing array module 
 import array 
@@ -265,7 +275,6 @@ print (arr.index(2))
 # using index() to print index of 1st occurrenece of 1 
 print ("The index of 1st occurrence of 1 is : ", end ="") 
 print (arr.index(1)) 
-
 
 print("\n********************************************************************************************************************************")
 
@@ -302,6 +311,5 @@ arr[4] = 8
 print("Array after updation : ", end ="") 
 for i in range (0, 6): 
 	print (arr[i], end =" ") 
-
 
 print("\n********************************************************************************************************************************")
